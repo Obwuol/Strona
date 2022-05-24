33dfb563fe8e166ccf9113e5624a3ac42e57e0da
Obwuol <robert.cywko@student.pcez.pl>
2022-05-24 17:47:28 +0200

    Tabela v10

--- js/pierwiastki.js
+++ js/pierwiastki.js
@@ -3,7 +3,7 @@ var elementChoosed = [] ;
 var answer = "";
 var y = 0 ;
 var k=0 ;
-h = 0 ;
+var scor = 0 ;
 var point = 0;
 container = document.getElementsByClassName("symbol");
 for(x = 0; x < container.length; x++){
@@ -15,11 +15,12 @@ for(x = 0; x < container.length; x++){
       y++ ;
   }
 }
-document.getElementById("score").classList.remove("hide");
+document.getElementById("score").className = "hide"
 function randomElements(){
 if(elementChoosed.length < elements.length){
 random = Math.floor(Math.random() * elements.length) ;
 answer = elements[random][1];
+ele = elements[random][0];
 randomElement = elementsTable[random].parentNode ;
 if (elementChoosed.includes(random)){
        randomElements();
@@ -45,8 +46,10 @@ function gameStart(e){
   document.getElementById("startButton").className = "hide"
   document.getElementById("inputAuto").className = "show"
   document.getElementById("resetButton").className = "show btn btn-info"
-  document.getElementById("score").className.add("show");
-  
+  document.getElementById("score").className = "show"
+  document.getElementById("timer").className = "show"
+  document.getElementById("score").innerHTML ="<div> Wynik: "+scor+"</div>"  ;
+  countDownDate = new Date().getTime()+1000*10*30;
 }
 
 function checkQuestion(event){
@@ -56,8 +59,8 @@ function checkQuestion(event){
       randomElement.classList.remove("checked");
       randomElement.classList.add("true");
       randomElement.classList.add("pink");
-      h++ ;
-      document.getElementById("score").innerHTML = h ;
+      scor++ ;
+      document.getElementById("score").innerHTML ="<div> Wynik: "+scor+"</div>"  ;
     }
     else {
       randomElement.classList.remove("checked");
@@ -82,6 +85,44 @@ function resetGame(e){
   document.getElementById("startButton").className = "btn btn-secondary show";
   document.getElementById("inputAuto").className = "hide";
   document.getElementById("resetButton").className = "hide";
-  document.getElementById("score").innerHTML = h=0 ;
-  document
+  document.getElementById("score").className = "show"
+  document.getElementById("timer").className = "hide"
+  scor=0
+  document.getElementById("score").innerHTML =""  ;
 }
+//TIMER
+var countDownDate = new Date().getTime()+1000*10*30;
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var minutes = Math.floor((distance % (1000 * 10 * 30)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+  if(scor == 1){
+    document.getElementById("chance").innerHTML = "Wybierz pierwiastek" ;
+    document.getElementsByClassName("checked").className = "pink"
+    document.getElementById("inputElem").className = "show"
+    inputElem = document.getElementById("inputElem").value
+}
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "KONIEC GRY";
+  }
+}, 1000);
+  function checkEle(event){
+    if(event.keyCode == 13){
+      inputElem = document.getElementById("inputElem").value
+      for(x= 0 ; x < elements.length ; x++) {
+        if(inputElem == elements[x][0]){
+              elementsTable[x].parentNote.className = "checked";
+        }
+
+      }
+    }
+  }
